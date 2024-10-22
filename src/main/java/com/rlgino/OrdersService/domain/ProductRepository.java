2166f4f4e204b8d5f83b52283321c808fb3ad849
Gino Luraschi <Gino.Luraschi@plantandfood.co.nz>
2024-10-22 15:36:21 +1300

    feat: Add repository to keep consistency

--- src/main/java/com/rlgino/OrdersService/domain/ProductRepository.java
+++ src/main/java/com/rlgino/OrdersService/domain/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.rlgino.OrdersService.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, UUID> {
 }
